d2bb8f6446a014372935af99ff0a355925260799
Kaleparth <147709370+Kaleparth@users.noreply.github.com>
2023-10-12 15:56:18 +0530

    Create averagesum.cpp

---
+++ averagesum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n1,n2,n3,n4,n5;
+sum=0;
+avg=0;
+cout<<"enter first number"<<"\n";
+cin>>n1;
+cout<<"enter second number"<<"\n";
+cin>>n1;
+cout<<"enter third number"<<"\n";
+cin>>n1;
+cout<<"enter four number"<<"\n";
+cin>>n1;
+cout<<"enter five number"<<"\n";
+cin>>n1;
+sum=n1+n2+n3+n4+n5;
+avg=sum/5
+cout<<"average is:"<<avg;
+return 0;
+}
